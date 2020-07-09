a72df2616b476d73078b67a59f3f184f95aa558b
Rizky <rizkyfirmansyah967@gmail.com>
2020-07-09 16:52:05 +0700

    create card vendor component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+      class="container mx-auto grid grid-cols-none grid-flow-row-dense sm:grid-cols-2 xl:grid-cols-4 gap-5"
+    >
+      <VendorCard v-for="vendor in vendors" :key="vendor.id" :vendor="vendor" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VendorCard from "./components/VendorCard";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    VendorCard
+  },
+  data() {
+    return {
+      vendors: []
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      fetch("https://my-json-server.typicode.com/rizuki96/jsontest/vendors")
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          this.vendors = data;
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/VendorCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container mx-auto card bg-red-100 max-w-sm">
+    <div class="container mx-auto px-6">
+      <div class="flex flex-col items-center p-10">
+        <div class="w-40 h-40 rounded-full bg-black">
+          <img src alt />
+        </div>
+        <div class="pt-8">
+          <h1 class="font-semibold text-lg text-green-400">Sirih Gading Catering</h1>
+          <ul class="flex items-center justify-center">
+            <li>
+              <a href>Catering</a>
+            </li>
+            <span class="mx-2">|</span>
+            <li>
+              <a href>Bandung, ID</a>
+            </li>
+          </ul>
+          <div>
+            rating
+            <span>4.6/5</span>
+          </div>
+          <div></div>
+          <div>
+            <button>
+              <a href>$$$</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vendor",
+  props: ["vendor"]
+};
+</script>
